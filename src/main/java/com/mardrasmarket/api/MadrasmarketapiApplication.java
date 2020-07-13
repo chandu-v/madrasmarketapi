4eec48a0d3bc7301a176b2252b1997c286c3c981
Chandu <chandu_boss@hotmail.com>
2020-07-13 22:58:57 +0530

    Initial Commit

---
+++ src/main/java/com/mardrasmarket/api/MadrasmarketapiApplication.java
@@ -0,0 +1,13 @@
+package com.mardrasmarket.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MadrasmarketapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MadrasmarketapiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mardrasmarket/api/MadrasmarketapiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mardrasmarket.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MadrasmarketapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
